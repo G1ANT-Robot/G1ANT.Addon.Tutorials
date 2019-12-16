ab3d0e581a4eae373a51e926683a13880d7384ce
Chris Prusik <chris@g1ant.com>
2019-12-16 12:02:12 +0100

    G1ANT.Robot.Api.Orchestrator

---
+++ G1ANT.Robot.Api.Orchestrator/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace G1ANT.Robot.Api.Orchestrator.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
